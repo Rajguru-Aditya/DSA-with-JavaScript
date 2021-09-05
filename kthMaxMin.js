32883c2c37e733ed130508cd96995cbbf4b0f126
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-09-06 00:33:21 +0530

    Program to find kth min and max element from an array

---
+++ kthMaxMin.js
@@ -0,0 +1,37 @@
+const k = 2;
+let myArr = [5, 8, 3, 9, 7, 4];
+
+const minNmax = (arr) => {
+  // Sort array
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++)
+      if (arr[i] < arr[j]) {
+        console.log(
+          "Index i:",
+          i,
+          "Index j:",
+          j,
+          "value arr[i]:",
+          arr[i],
+          "value arr[j]:",
+          arr[j],
+          "\nvalue of arr:",
+          arr
+        );
+        let x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+        console.log("After changing elements:", arr);
+      }
+  }
+  let newArr = [];
+  // Reverse sorted array
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  let minTerm = arr[k - 1];
+  let maxTerm = newArr[k - 1];
+  return `Kth min element: ${minTerm}, Kth max element: ${maxTerm}`;
+};
+
+console.log(minNmax(myArr));
